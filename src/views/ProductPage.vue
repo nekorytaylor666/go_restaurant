1eebe3c7bbcc386d8efb9d878b5addfd60270ddd
Tokhtar Akhmetov <t.akhmetov@chocotravel.com>
2021-04-08 19:23:45 +0600

    f

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="order-container">
-    <div class="relative">
+    <div class="relative row-start-1 row-end-3 col-start-1">
       <div class="w-full absolute top-0 h-16 bg-gradient-to-b from-black opacity-60"></div>
       <img class="w-full h-72 object-cover" src="../assets/images/soup.jpg" alt="soup" />
     </div>
-    <div class="bg-yellow p-4 rounded-xl z-10">
+    <div class="bg-yellow p-4 rounded-xl z-10 row-start-2 row-end-4 col-start-1">
       <div class="flex flex-col items-center my-4">
         <div class="border border-green rounded-full p-2 w-10 h-10 flex items-center justify-center mb-4">
           <span class="block text-green font-display font-bold text-lg mb-1">01</span>
@@ -43,6 +43,6 @@ export default {
 .order-container {
   display: grid;
   min-height: 100vh;
-  grid-template-rows: minmax(40%, 300px) 60%;
+  grid-template-rows: minmax(40%, 300px) 1rem 60%;
 }
 </style>
